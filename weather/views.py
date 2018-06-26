8e68a094220b85b4628076a573f1881115a509ac
Uroš Jevremović <jevremovic.uros91@gmail.com>
2018-06-26 13:41:43 +0200

    added form for user input

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,19 @@
 import requests
+import json
+
 from django.shortcuts import render
 
 from .models import City
+from .forms import CityForm
 
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=464a1d9339dc2282caf9b17e02224f3b'
+
+    if request.method == 'POST':
+        pass
+
+    form = CityForm()
 
     cities = City.objects.all()
 
@@ -16,12 +24,15 @@ def index(request):
         response = requests.get(url.format(city)).json()
 
         city_weather = {
-            'city': city,
+            'city': city.name,
             'temperature': response['main']['temp'],
             'description': response['weather'][0]['description'],
             'icon': response['weather'][0]['icon'],
         }
+        # print(city_weather)
+        # city_weather = json.dumps(city_weather)
+        # print(city_weather)
         weather_data.append(city_weather)
 
-    print(weather_data)
-    return render(request, 'weather/weather.html', context={'weather_data': weather_data})
+    # print(weather_data)
+    return render(request, 'weather/weather.html', context={'weather_data': weather_data, 'form':form})
